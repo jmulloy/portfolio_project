3dd27d49d162e37c44752c21d6b714004b5da9b9
github username <github email address>
2019-05-14 10:12:52 -0400

    Removed commented out code from Welcome.js

--- src/componenets/welcome.js
+++ src/componenets/welcome.js
@@ -1,11 +1,11 @@
-// import React from 'react'
+import React from 'react'
 
-// const Welcome = () => {
-//     return (
-//       <div className='welcome-page'>
-//         <h1>Welcome to my Portfolio!</h1>
-//       </div>
-//     )
-//   }
+const Welcome = () => {
+    return (
+      <div className='welcome-page'>
+        <h1>Welcome to my Portfolio!</h1>
+      </div>
+    )
+  }
 
-//   export default Welcome
+  export default Welcome
