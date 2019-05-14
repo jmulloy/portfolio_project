cb5d4a7d35e33df448c92aff8f269cc863fba5b4
github username <github email address>
2019-05-14 10:18:47 -0400

    Edited the index.js file in order to properly set up routing for first 2 components created

--- src/App.js
+++ src/App.js
@@ -4,25 +4,40 @@ import Welcome from './components/Welcome.js'
 import About from './components/About.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      {/* <Header className="Header"/> */}
+      {/* <Switch> */}
+        <Route exact path='/' component={Welcome} />
+        <Route exact path='/about' component={About} />
+      {/* </Switch> */}
+        
+      </div>
+    );
+  }
 }
 
 export default App;
