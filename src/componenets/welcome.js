5b105c5770947cace24c6de83316c9205607b1a9
github username <github email address>
2019-05-14 09:39:02 -0400

    Added components folder and built out welcome component

---
+++ src/componenets/welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+    return (
+      <div className='welcome-page'>
+        <h1>Welcome to the Tidder React App</h1>
+      </div>
+    )
+  }
+
+  export default Welcome
