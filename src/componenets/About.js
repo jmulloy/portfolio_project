528b8bb53d56139c784ee264879d89248663b52c
github username <github email address>
2019-05-14 09:42:02 -0400

    Added my about section for application

---
+++ src/componenets/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+      <div className='about-page'>
+
+        <h2>This is my personal portfolio website to showcase some of the applications I have built and worked on through mjy Full Stack Web Development course.</h2>
+        <h3>You may use the linkls in the NavBar to navigate through the application!</h3>
+       
+      </div>
+    )
+  }
+
+  export default About
