989d9c77af6837eb71282156ee928b72ee65cd5a
github username <github email address>
2019-05-20 09:35:30 -0400

    Added/installed bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import { Navbar, Nav, NavItem } from 'react-bootstrap';
-// import { Link } from 'react-router-dom'
+import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 
-// class Header extends React.Component {
-//     render() {
-//         return (
-//             <Navbar>
+class Header extends React.Component {
+    render() {
+        return (
+            <Navbar>
              
-//               <Nav>
-//               <NavItem eventKey={2}><Link to='/'>Home</Link></NavItem>
+              <Nav>
+              <NavItem eventKey={2}><Link to='/'>Home</Link></NavItem>
 
-//                   <NavItem eventKey={2}><Link to='/about'>About</Link></NavItem>
+                  <NavItem eventKey={2}><Link to='/about'>About</Link></NavItem>
                   
-//               </Nav>
+              </Nav>
             
-//             </Navbar>
-//         )
-//     }
-// }
-// export { Header }
+            </Navbar>
+        )
+    }
+}
+export { Header }
