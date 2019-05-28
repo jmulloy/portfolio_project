34df58ad7a97089aa0a1885a7ffaed375d9ecb91
github username <joshmulloy1@gmail.com>
2019-05-28 15:06:30 -0400

    Fixed spelling error in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
       <div className='about-page'>
 
-        <h2>This is my personal portfolio website to showcase some of the applications I have built and worked on through mjy Full Stack Web Development course.</h2>
+        <h2>This is my personal portfolio website to showcase some of the applications I have built and worked on through my Full Stack Web Development course.</h2>
         <h3>You may use the links in the NavBar to navigate through the application!</h3>
        
       </div>
