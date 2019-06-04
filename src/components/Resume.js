e301de100357a197a3d7a21c499fd20560644649
jmulloy <joshmulloy1@gmail.com>
2019-06-04 09:24:47 -0400

    Added Resume div to Resume file

---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+      <div className='resume-page'>
+
+        <h2>Here are my resumes. </h3>
+       
+      </div>
+    )
+  }
+
+  export default Resume
